b4722930e5101c8dfce8ae592a02872cc761302c
Spotman <i.am@spotman.ru>
2016-09-24 21:20:36 +0400

    migrations:renameInfoColumn task for renaming migration table "info" column

---
+++ classes/Kohana/Task/Migrations/RenameInfoColumn.php
@@ -0,0 +1,26 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Creates a new migration file
+ *
+ * @package    Minion/Migrations
+ * @category   Helpers
+ * @author     Denis Terekhov (https://github.com/spotman)
+ * @copyright  (c) 2016-2017 Spotman
+ * @license    BSD 3 http://opensource.org/licenses/BSD-3-Clause
+ */
+class Kohana_Task_Migrations_RenameInfoColumn extends Minion_Task {
+
+	protected function _execute(array $params)
+	{
+        $config = Kohana::$config->load('migrations')
+            ->as_array();
+
+        $table = $config['table'];
+
+        DB::query(Database::SELECT, "ALTER TABLE `$table` CHANGE `info` `description` TEXT NOT NULL DEFAULT '' AFTER `name`;");
+
+		Minion_CLI::write('Done!');
+	}
+
+} // End Kohana_Task_Migrations_Create
