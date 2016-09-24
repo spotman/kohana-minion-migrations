111481793c3183a732ca4646d908a37aab0b3dc1
Spotman <i.am@spotman.ru>
2016-09-24 20:07:32 +0400

    rename migrations.info column to migrations.description + add before_up() / before_down() / after_up() / after_down() methods to Kohana_Migration class + add MySQLi schema

---
+++ schemas/migrations/MySQLi/install.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `:prefix_migrations` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `filename` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` TEXT NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `migrations_uniq_filename` (`filename`),
+  KEY `migrations_date` (`date`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
